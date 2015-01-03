8f3e6310ae46587f937c375a1a3ef269c2a48bc6
Marian Rusnak <grizly21@gmail.com>
2015-01-03 18:43:08 +0100

    Added MySQL support for EntityFramework

---
+++ EasyBoni/Models/MySQL/MySqlInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+
+namespace EasyBoni.Models.MySQL
+{
+    public class MySqlInitializer : IDatabaseInitializer<ApplicationDbContext>
+    {
+        public void InitializeDatabase(ApplicationDbContext context)
+        {
+            if (!context.Database.Exists())
+            {
+                // if database did not exist before - create it
+                context.Database.Create();
+            }
+            else
+            {
+                // query to check if MigrationHistory table is present in the database 
+                var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
+                string.Format(
+                  "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '{0}' AND table_name = '__MigrationHistory'",
+                  "EasyBoni"));
+
+                // if MigrationHistory table is not there (which is the case first time we run) - create it
+                if (migrationHistoryTableExists.FirstOrDefault() == 0)
+                {
+                    context.Database.Delete();
+                    context.Database.Create();
+                }
+            }
+        }
+    }
+}
