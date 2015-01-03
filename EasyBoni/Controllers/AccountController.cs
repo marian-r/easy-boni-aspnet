f675dd3c7e9d453976c921d54e201d4cbb8bbf2b
Marian Rusnak <grizly21@gmail.com>
2015-01-03 18:45:53 +0100

    Added new user properties

--- EasyBoni/Controllers/AccountController.cs
+++ EasyBoni/Controllers/AccountController.cs
@@ -151,7 +151,13 @@ namespace EasyBoni.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { 
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
