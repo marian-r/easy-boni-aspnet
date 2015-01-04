c49d118b5b721ea7513895a0d05073595e24d61d
Marian Rusnak <grizly21@gmail.com>
2015-01-04 18:01:33 +0100

    Account management cleanup

--- EasyBoni/Models/AccountViewModels.cs
+++ EasyBoni/Models/AccountViewModels.cs
@@ -15,30 +15,6 @@ namespace EasyBoni.Models
         public string ReturnUrl { get; set; }
     }
 
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
-
-    public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
-
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
-
-        [Display(Name = "Remember this browser?")]
-        public bool RememberBrowser { get; set; }
-
-        public bool RememberMe { get; set; }
-    }
-
     public class ForgotViewModel
     {
         [Required]
